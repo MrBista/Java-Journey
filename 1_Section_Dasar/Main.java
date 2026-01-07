523610777540550a51b6ac4a7164e3f41e9f366a
MrBista <gustibisman2308@gmail.com>
2026-01-07 22:08:04 +0700

    learn OOP In depth

--- 1_Section_Dasar/Main.java
+++ 1_Section_Dasar/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        TipeData.NumberTipeData();
-        TipeData.TipeDataString();
-        TipeData.TipeDataBoolean();
-        TipeData.TipeDataChar();
+//        TipeData.NumberTipeData();
+//        TipeData.TipeDataString();
+//        TipeData.TipeDataBoolean();
+//        TipeData.TipeDataChar();
+        TipeData.tipdDataArray();
 
         variabelTest();
     }
