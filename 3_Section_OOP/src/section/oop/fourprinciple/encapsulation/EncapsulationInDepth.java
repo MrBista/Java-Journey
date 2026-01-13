8350743fddd05efa8550cff6abc1fabc544bd20b
MrBista <gustibisman2308@gmail.com>
2026-01-13 20:46:53 +0700

    learn encapsulation

---
+++ 3_Section_OOP/src/section/oop/fourprinciple/encapsulation/EncapsulationInDepth.java
@@ -0,0 +1,11 @@
+package src.section.oop.fourprinciple.encapsulation;
+
+public class EncapsulationInDepth {
+    public static void main(String[] args) {
+        System.out.println("Hello World gengs");
+        // gambang banget cuma perkara hiding property atau field
+        // bisa pakai public(semua bisa akses), private(hanya class itu aja best practice), protected(satu module)
+
+
+    }
+}
