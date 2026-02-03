ab9510dbc8cab60c4d3bf5eee56f55f0acd2e0bd
MrBista <gustibisman2308@gmail.com>
2026-02-03 21:30:44 +0700

    learn jdbc phase 4

--- 12_Section_Java_Databases/database-learn/src/test/java/com/bisma/foundation/TransactionDbTest.java
+++ 12_Section_Java_Databases/database-learn/src/test/java/com/bisma/foundation/TransactionDbTest.java
@@ -1,4 +1,34 @@
 package com.bisma.foundation;
 
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class TransactionDbTest {
+
+    @Test
+    void testManualTransaction() throws SQLException {
+        String sqlInsertUser = """
+                insert into users(username, password, name) values(?, ?, ?);
+                """;
+
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+        connection.setAutoCommit(false);
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sqlInsertUser);
+
+
+        preparedStatement.setString(1, "MrBistaGagah2308Abc");
+        preparedStatement.setString(2, "Abc@1234");
+        preparedStatement.setString(3, "Gusti Bisman Taka");
+
+        preparedStatement.executeUpdate();
+
+        connection.commit();
+        preparedStatement.close();
+        connection.close();
+    }
+
 }
