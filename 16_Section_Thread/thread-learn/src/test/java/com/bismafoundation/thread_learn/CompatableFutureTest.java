642d45300da2aa924c25ab10c42a584e398907e8
MrBista <gustibisman2308@gmail.com>
2026-02-20 23:55:01 +0700

    learn thread java in depth completablefuture 2

--- 16_Section_Thread/thread-learn/src/test/java/com/bismafoundation/thread_learn/CompatableFutureTest.java
+++ 16_Section_Thread/thread-learn/src/test/java/com/bismafoundation/thread_learn/CompatableFutureTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.*;
 
 public class CompatableFutureTest {
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
 
 
     CompletableFuture<String> basicCompatableFuture() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
             return "C Cumber isi";
@@ -27,4 +27,51 @@ public class CompatableFutureTest {
 
         Assertions.assertEquals("C Cumber isi", resultComplateableFuture);
     }
+
+    public Future<String> getValue() {
+        CompletableFuture<String> future = new CompletableFuture<>();
+
+        executorService.submit(() -> {
+            try {
+                Thread.sleep(2_000);
+                future.complete("Success");
+
+            } catch (InterruptedException e) {
+                future.completeExceptionally(e);
+            };
+        });
+
+        return future;
+    }
+
+    public Future<String> getValueExceptaion() {
+        CompletableFuture<String> future = new CompletableFuture<>();
+
+        executorService.submit(() -> {
+            try {
+                Thread.sleep(2_000);
+                throw new InterruptedException("interupted error exception");
+
+            } catch (InterruptedException e) {
+                future.completeExceptionally(e);
+            };
+        });
+
+        return future;
+    }
+
+    @Test
+    void testGetValueCompleteableFuture() throws ExecutionException, InterruptedException {
+        String result = getValue().get();
+
+        Assertions.assertEquals("Success", result);
+    }
+    @Test
+    void testGetValueExceptionCompleteableFuture() throws ExecutionException, InterruptedException {
+        String result = getValueExceptaion().get();
+
+        System.out.println(result);
+    }
+
+
 }
